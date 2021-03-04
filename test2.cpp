2d2522fe13eee2c17baaf5ce5d3434ed45d9db4a
owen0621 <owen10270936@gmail.com>
2021-03-04 19:49:47 +0800

    init commit

---
+++ test2.cpp
@@ -0,0 +1,11 @@
+/*************************************************************************
+  > File Name: test2.cpp
+  > Author: Wang
+  > Mail: owen10270936@gmail.com 
+  > Created Time: Thu Mar  4 19:48:26 2021
+ ************************************************************************/
+
+//#include <iostream>
+//#include <cstdlib>
+//using namespace std;
+
